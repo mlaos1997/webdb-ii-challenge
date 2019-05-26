b2519a246529548fdb9f230990105987f36524cd
Marcelo Laos <mlaos1997@gmail.com>
2019-05-26 14:06:12 -0400

    setup knex.js

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
 
+const knexConfig = {
+  client: 'sqlite3',
+  connection: {
+    filename: './data/lambda.sqlite3' //from the root folder
+  },
+  useNullAsDefault: true
+};
+
+const db = knex(knexConfig);
 const server = express();
 
 server.use(express.json());
 server.use(helmet());
 
-// endpoints here
+// endpoints heres
+server.get('/', (req, res) => {
+  db('zoos')
+  .then(response => {
+    res.status(200).json(response);
+  })
+  .catch(err => console.log(err));
+});
 
 const port = 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
+
+
