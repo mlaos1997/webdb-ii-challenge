ffe07077e0dcff31f0130fa35f2b2778b249df3f
Marcelo Laos <mlaos1997@gmail.com>
2019-05-26 18:59:47 -0400

    get and post routes complete

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const helmet = require('helmet');
 const knex = require('knex');
 
 const knexConfig = {
-  client: 'sqlite3',
-  connection: {
-    filename: './data/lambda.sqlite3' //from the root folder
-  },
-  useNullAsDefault: true
+    client: 'sqlite3',
+    connection: {
+        filename: './data/lambda.sqlite3' //from the root folder
+    },
+    useNullAsDefault: true
+    // debug : true
 };
 
 const db = knex(knexConfig);
@@ -17,17 +18,52 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints heres
-server.get('/', (req, res) => {
-  db('zoos')
-  .then(response => {
-    res.status(200).json(response);
-  })
-  .catch(err => console.log(err));
+server.get('/api/zoos', (req, res) => {
+    db('zoos') // returns a promise with all the rows
+        .then(response => {
+        res
+            .status(200)
+            .json(response);
+    }).catch(err => {
+        console.log(err)
+        res
+            .status(500)
+            .json(err);
+    });
+});
+
+server.get('/api/zoos/:id', (req, res) => {
+    db('zoos')
+        .where({id: req.params.id})
+        .first() // will return us an object instead of an array
+        .then(zoo => {
+            res
+                .status(200)
+                .json(zoo)
+        })
+        .catch(err => {
+            res
+                .status(500)
+                .json(err);
+        })
+});
+
+server.post('/api/zoos', (req, res) => {
+    // INSERT INTO Zoos (title) VALUES ('Slaughterhouse Five'); second argument for
+    // insert is what we want returned from the promise, in this case we want the
+    // name back
+    db('zoos')
+        .insert(req.body, ['id'])
+        .then(response => res.status(200).json(response))
+        .catch(err => {
+            console.log(err)
+            res
+                .status(500)
+                .json(err);
+        });
 });
 
 const port = 3300;
-server.listen(port, function() {
-  console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
+server.listen(port, function () {
+    console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
-
-
